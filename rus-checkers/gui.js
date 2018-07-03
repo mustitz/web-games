f2918306d239209debbf65fb3a9d72ffc8a1fc62
Mustitz <mustitz@gmail.com>
2018-07-03 08:24:56 +0300

    Add HTML template for using JavaScript.
    Also a JavaScript stub was added.

---
+++ rus-checkers/gui.js
@@ -0,0 +1,13 @@
+var yooRusCheckers = new function() {
+
+var initPublic = function(obj) {
+    obj.init = init;
+};
+
+var init = function(args) {
+    console.log('Not implemeted');
+};
+
+initPublic(this);
+
+}; // yooRusCheckers
